7e9887ec8a278c4ace7f156abdc87a12647d193a
Tmyk-Ichi <tmyk.ichikawa@gmail.com>
2019-12-04 09:11:28 +0000

    [Create] Login_form Mypage&Mypage_edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+	#diviseの機能が使われる場合、その前に実行
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	#ログイン後のページを設定
+	def after_sign_in_path_for(resource)
+		if current_user
+        flash[:notice] = "ログインに成功しました"
+		mypage_path(resource)
+	    end
+	end
+
+	#ログアウト後のページを設定
+	def after_sign_out_path_for(resource)
+		new_user_session_path
+	end
+
+	protected
+	#ユーザ登録（sign_up）の際に、ユーザ名（name）のデータ操作を許可
+    def configure_permitted_parameters
+    	devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
+
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '2a258e2593b001ccf362f9aa3162e09bd5f79de0966c7301403d795dce31e5b6fe197a63730b91e8dd713b77ff5e6558e20c097abb876995ff5493e43ae2514b'
